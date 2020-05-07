76d47b71c920ba9f3a3f77c7d87f1bf9f236e3b0
Mai Nour Al-Deen <mai.mohamed339@gmail.com>
2020-05-07 21:05:27 +0200

    initialize repository
    sample data, model

---
+++ recurrent_model.py
@@ -0,0 +1,138 @@
+from tqdm import tqdm
+import numpy as np
+import cv2 as cv
+import os
+from keras_preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Activation, GRU, Dropout, TimeDistributed, Dense, Bidirectional, Conv3D, Flatten, MaxPooling3D, \
+    ZeroPadding3D
+
+"""**Global Variables**"""
+
+y_labels = []
+all_videos = []
+max_no_frames = 0
+
+training_folder = "Training_Set"
+testing_folder = "Testing_set"
+
+"""**Get max number of frames and saving all videos without duplication in (total_frames)**"""
+
+
+# read data and labeling, return max number in all frames
+def read_data():
+    global all_videos
+    global max_no_frames
+    global y_labels
+
+    for folder in tqdm(os.listdir(training_folder)):  # folder: Basketball, Diving, Jumping, Tennis, and Walking
+        folder_path = os.path.join(training_folder, folder)
+        print("-->In folder:", folder)
+        # check_counter = 0
+        label = -1
+        # naming creiteria:
+        if (folder == "Diving"):
+            label = 0
+        elif (folder == "Jumping"):
+            label = 1
+        elif (folder == "Basketball"):
+            label = 2
+        elif (folder == "Tennis"):
+            label = 3
+        elif (folder == "Walking"):
+            label = 4
+
+        for video in os.listdir(folder_path):
+
+            video_path = os.path.join(folder_path, video)
+            cap = cv.VideoCapture(video_path)
+            video_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+
+            max_no_frames = max(max_no_frames, video_frames)
+
+            one_video_frames = []
+            one_video_frames = np.asarray(one_video_frames)
+            leave_frame = 0
+            while (True):
+                if leave_frame % 2 == 0:
+                    ret, frame = cap.read()
+                    if ret == False:
+                        break
+                    frame = cv.resize(frame, (150, 150))
+                    frame = frame.reshape(150, 150, 3)
+                    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+                    one_video_frames = np.append(one_video_frames, frame)
+                    one_video_frames = np.reshape(one_video_frames, (-1, frame.shape[0], frame.shape[1]))
+                else:
+                    leave_frame += 1
+
+                # all_videos.append(np.array([one_video_frames, label]))
+            all_videos.append(one_video_frames)
+            y_labels.append(label)
+    print("max # of frames:", max_no_frames)
+
+    # all_videos: each element "has a diff size according to the length of each video" is a list of frames for each video
+    np.save('all_videos.npy', all_videos)
+    np.save('y_labels.npy', y_labels)
+    np.save('max_no_frames.npy', max_no_frames)
+
+
+def main():
+    global all_videos
+    global y_labels
+    global max_no_frames
+    save_path = 'all_videos.npy'
+    if (os.path.exists(save_path)):
+        all_videos = np.load(save_path, allow_pickle=True)
+        y_labels = np.load('y_labels.npy', allow_pickle=True)
+        max_no_frames = np.load('max_no_frames.npy', allow_pickle=True)
+        print("train data loaded")
+    else:
+        read_data()
+        print("train data created")
+
+    padded_videos = []
+    padded_videos = pad_sequences(all_videos, maxlen=max_no_frames, padding='pre')
+    # padded_videos=pad_sequences(all_videos,padding='pre')# bt3rf lw7dha el max lenght <3
+    print(len(padded_videos[1]))
+    # print(all_videos.shape)
+
+    ##################################################
+    # model
+
+    shape = (259, 150, 150, 3)
+    model = Sequential()
+
+    # model.add(TimeDistributed(ZeroPadding3D(padding=(1, 2, 2), input_shape=shape)))
+    model.add(Conv3D(4, kernel_size=3, activation="relu", input_shape=shape))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=1, padding='valid'))
+    # model.add(Dropout(0.2))
+
+    # model.add(TimeDistributed(ZeroPadding3D(padding=(1, 2, 2))))
+    # model.add(Conv3D(8, kernel_size=3, activation="relu"))
+    # model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=1, padding='valid'))
+    # model.add(Dropout(0.2))
+
+    # # model.add(TimeDistributed(ZeroPadding3D(padding=(1, 1, 1))))
+    # model.add(Conv3D(12, kernel_size=3, activation="relu"))
+    # model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=1, padding='valid'))
+    # model.add(Dropout(0.2))
+
+    # model.add(TimeDistributed(Flatten()))
+
+    # model.add(Bidirectional(GRU(256, return_sequences=True), merge_mode='concat'))
+    #
+    # model.add(Bidirectional(GRU(256, return_sequences=True), merge_mode='concat'))
+    # as the model
+    model.add(Flatten())
+
+    model.add((Dense(128, activation='relu')))
+    model.add((Dense(5, activation='softmax')))
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    model.fit(padded_videos, y_labels, epochs=2, batch_size=max_no_frames, verbose=2)
+
+
+main()
---
+++ run_model.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH --job-name=gpu_devices
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=300:00:00
+#SBATCH --account=g.shams020
+
+# list-gpu-devices/list.sh (Slurm submission script)
+
+python recurrent_model.py
